26d6aee00c5e07a40e0a5253024510f9fc061620
BUsuper <merzlyakov96@gmail.com>
2025-11-28 14:18:05 +0300

    Implement basic routing using React Router

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,12 +1,16 @@
 import "./App.css";
+import { Route, Routes } from "react-router";
 import Home from "../pages/Home";
-import About from "../pages/About/About";
+import About from "../pages/About";
+import Login from "../pages/Login";
 
 function App() {
   return (
-    <div>
-      <About />
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
   );
 }
 
