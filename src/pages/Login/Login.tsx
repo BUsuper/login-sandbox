cea34a53ab5fdce37549a954a2fc01783f5520a4
BUsuper <merzlyakov96@gmail.com>
2025-11-30 09:54:53 +0300

    Add login form and logic to the Login page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,10 +1,65 @@
+import { Box, Button, TextField } from "@mui/material";
 import { Header } from "../../components/";
+import { useState, type ChangeEvent, type FormEvent } from "react";
 
 export default function Login() {
+  const [usernameInput, setUsernameInput] = useState<string>("");
+  const [passwordInput, setPasswordInput] = useState<string>("");
+
+  function handleUsernameInputChange(e: ChangeEvent<HTMLInputElement>): void {
+    setUsernameInput(e.target.value);
+  }
+
+  function handlePasswordInputChange(e: ChangeEvent<HTMLInputElement>): void {
+    setPasswordInput(e.target.value);
+  }
+
+  function handleSubmit(e: FormEvent<HTMLFormElement>): void {
+    e.preventDefault();
+    console.log(`Login: ${usernameInput}\nPassword: ${passwordInput}`);
+  }
+
   return (
     <>
       <Header></Header>
-      <p>Hi, it's Login</p>
+      <Box
+        id="loginForm"
+        component="form"
+        onSubmit={handleSubmit}
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <TextField
+          id="usernameInput"
+          required
+          value={usernameInput}
+          onChange={handleUsernameInputChange}
+          label="Username"
+          sx={{ my: 2 }}
+        ></TextField>
+        <TextField
+          id="passwordInput"
+          required
+          value={passwordInput}
+          onChange={handlePasswordInputChange}
+          label="Password"
+          type="password"
+        ></TextField>
+        <Button
+          id="loginSubmitButton"
+          variant="outlined"
+          type="submit"
+          sx={{ my: 2, width: 90 }}
+        >
+          Login
+        </Button>
+        <Button id="signupButton" variant="outlined" sx={{ width: 90 }}>
+          Sign up
+        </Button>
+      </Box>
     </>
   );
 }
