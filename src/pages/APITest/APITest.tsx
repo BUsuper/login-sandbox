bc24d3649127e30e7304703dc83d53af7d2f56ae
BUsuper <merzlyakov96@gmail.com>
2025-12-01 09:42:41 +0300

    Add Skeleton to avoid an empty page while getting a response from the API

--- src/pages/APITest/APITest.tsx
+++ src/pages/APITest/APITest.tsx
@@ -28,7 +28,9 @@ export default function APITest() {
 
   const [characterData, setCharactersData] = useState<Character[] | null>(null);
   const BASE_URL = "https://rickandmortyapi.com/api";
-  const CHARACTER_IDS_URL = `/character/${[1, 2, 3, 4, 5, 10, 22].join(",")}`;
+  const characterIds = [1, 2, 3, 4, 5, 10, 22];
+  const cahracterIdsUrl = `/character/${characterIds.join(",")}`;
+  const skeletonItemsCount = characterIds.length;
 
   async function fetchCharacterData(
     baseURL: string,
@@ -46,17 +48,15 @@ export default function APITest() {
   useEffect(() => {
     // This will be used to abort the request when the component (page) unmounts
     const ctrl = new AbortController();
-
     (async () => {
       try {
-        const data = await fetchCharacterData(BASE_URL, CHARACTER_IDS_URL);
+        const data = await fetchCharacterData(BASE_URL, cahracterIdsUrl);
         setCharactersData(data);
       } catch (err) {
         console.error(`Could not fetch the data: ${err}`);
         throw err;
       }
     })();
-
     // Abort the request when unmounted
     return () => ctrl.abort();
   }, []);
@@ -64,34 +64,43 @@ export default function APITest() {
   return (
     <>
       <Header />
-      {characterData ? (
-        <Grid container spacing={4}>
-          {characterData.map((character) => {
-            return (
-              <Grid size={{ xs: 12, sm: 6, md: 4 }} key={character.id}>
-                <Card>
-                  <CardMedia
-                    component="img"
-                    image={character.image}
-                    alt={character.name}
-                    sx={{ height: 120, objectFit: "contain" }}
-                  />
-                  <CardContent>
-                    <Typography>{`#${character.id}`}</Typography>
-                    <Typography>{character.name}</Typography>
-                    <Typography>{`Status: ${character.status}`}</Typography>
-                    <Typography>{`Species: ${character.species}`}</Typography>
-                    <Typography>{`Gender: ${character.gender}`}</Typography>
-                    <Typography>{`Place of origin: ${character.origin.name}`}</Typography>
-                  </CardContent>
-                </Card>
-              </Grid>
-            );
-          })}
-        </Grid>
-      ) : (
-        <Skeleton></Skeleton>
-      )}
+      <Grid container spacing={4}>
+        {characterData
+          ? characterData.map((character) => {
+              return (
+                <Grid size={{ xs: 12, sm: 6, md: 4 }} key={character.id}>
+                  <Card sx={{ minWidth: 220, minHeight: 400 }}>
+                    <CardMedia
+                      component="img"
+                      image={character.image}
+                      alt={character.name}
+                      sx={{ height: 120, objectFit: "contain" }}
+                    />
+                    <CardContent>
+                      <Typography>{`#${character.id}`}</Typography>
+                      <Typography>{character.name}</Typography>
+                      <Typography>{`Status: ${character.status}`}</Typography>
+                      <Typography>{`Species: ${character.species}`}</Typography>
+                      <Typography>{`Gender: ${character.gender}`}</Typography>
+                      <Typography>{`Place of origin: ${character.origin.name}`}</Typography>
+                    </CardContent>
+                  </Card>
+                </Grid>
+              );
+            })
+          : Array(skeletonItemsCount)
+              .fill(0)
+              .map((_, index) => (
+                // The index key is fine because this stuff is static
+                <Grid size={{ xs: 12, sm: 6, md: 4 }} key={index}>
+                  <Skeleton
+                    variant="rectangular"
+                    sx={{ minWidth: 220, minHeight: 400 }}
+                    animation="wave"
+                  ></Skeleton>
+                </Grid>
+              ))}
+      </Grid>
     </>
   );
 }
