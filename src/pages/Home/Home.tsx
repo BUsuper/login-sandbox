7c773afcece172bcb1167a56834fed093b9c4313
BUsuper <merzlyakov96@gmail.com>
2025-12-01 13:19:19 +0300

    Add basic functionality and styles to the Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,25 @@
+import { Typography, Link } from "@mui/material";
 import { Header } from "../../components/";
+import { Link as RouterLink } from "react-router";
 
 export default function Home() {
+  const currentUser = null;
+
   return (
     <>
       <Header></Header>
-      <p>Hi, it's Home</p>
+      <Typography my={4} variant="h2">
+        Wellcome{currentUser ? `, ${currentUser}!` : "!"}
+      </Typography>
+      {currentUser ? (
+        <Link component={RouterLink} to="/api" underline="hover" variant="h5">
+          Check out the characters
+        </Link>
+      ) : (
+        <Link component={RouterLink} to="/login" underline="hover" variant="h5">
+          Log in
+        </Link>
+      )}
     </>
   );
 }
