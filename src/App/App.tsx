85338aebaa4f4db8c673c7e9ce45f5eba5fa1e4e
BUsuper <merzlyakov96@gmail.com>
2025-12-02 10:18:31 +0300

    Wrap the pages in the context provider

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,15 +1,21 @@
 import "./App.css";
 import { Route, Routes } from "react-router";
+import { useState } from "react";
 import { Home, About, Login, APITest } from "../pages/";
+import { UserContext } from "../context/UserContext";
+import type { User } from "../types/types";
 
 function App() {
+  const [currentUser, setCurrentUser] = useState<User | null>(null);
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/api" element={<APITest />} />
-    </Routes>
+    <UserContext value={{ currentUser, setCurrentUser }}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/api" element={<APITest />} />
+      </Routes>
+    </UserContext>
   );
 }
 
