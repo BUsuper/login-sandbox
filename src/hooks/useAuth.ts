406ff0eaaab0af201382a94b8c263e13168bc45b
BUsuper <merzlyakov96@gmail.com>
2025-12-04 17:30:20 +0300

    Fix useAuth (now unpacks currentUser from context and returns null if no user

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -2,9 +2,9 @@ import { useContext } from "react";
 import { UserContext } from "../context/UserContext";
 
 export function useAuth() {
-  const user = useContext(UserContext);
+  const { currentUser: user } = useContext(UserContext);
   if (!user) {
-    throw new Error("User is not authenticated");
+    return null;
   }
   return user;
 }
