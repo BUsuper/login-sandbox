eae7702d3416ab7223e7ab09fb35a2085b7ee404
BUsuper <merzlyakov96@gmail.com>
2025-12-04 14:06:29 +0300

    Change currentUser initialization to undefined, set it to null and logout (clear user and access token) after a failed refresh

--- src/context/UserContext.ts
+++ src/context/UserContext.ts
@@ -2,6 +2,6 @@ import { createContext } from "react";
 import type { UserContextValues } from "../types/types";
 
 export const UserContext = createContext<UserContextValues>({
-  currentUser: null,
+  currentUser: undefined,
   setCurrentUser: () => {},
 });
--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -3,16 +3,20 @@ import { useEffect, useState } from "react";
 import { UserContext } from "../context/UserContext";
 import type { User } from "../types/types";
 import { TokenContext } from "../context/TokenContext";
-import { refresh } from "../utils/auth";
+import { logout, refresh } from "../utils/auth";
 
 export function AuthProvider({ children }: { children: ReactNode }) {
-  const [currentUser, setCurrentUser] = useState<User | null>(null);
+  const [currentUser, setCurrentUser] = useState<User | null | undefined>(
+    undefined
+  );
   const [accessToken, setAccessToken] = useState<string | null>(null);
 
   useEffect(() => {
     const refreshToken = sessionStorage.getItem("refreshToken");
     if (refreshToken) {
       refresh(setAccessToken, setCurrentUser, refreshToken);
+    } else {
+      logout(setAccessToken, setCurrentUser);
     }
   }, []);
 
