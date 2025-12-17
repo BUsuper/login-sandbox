efd40bea6ce419e52895e39a9f829d714d25ea3e
BUsuper <merzlyakov96@gmail.com>
2025-12-17 10:23:15 +0300

    Add and style and svg warning icon to the Logout page

--- src/pages/Logout/Logout.tsx
+++ src/pages/Logout/Logout.tsx
@@ -1,12 +1,18 @@
-import { Button } from "@mui/material";
+/// <reference types="vite-plugin-svgr/client" />
+// This is necessary for the vite svgr plugin to work with TS
+
+import { Box, Button, SvgIcon } from "@mui/material";
 import { Header } from "../../components";
 import { logout } from "../../utils/auth";
 import { useContext } from "react";
+import { useTheme } from "@mui/material/styles";
 import { TokenContext } from "../../context/TokenContext";
 import { UserContext } from "../../context/UserContext";
 import { useNavigate } from "react-router";
+import Warning from "../../assets/warning.svg?react";
 
 export function Logout() {
+  const theme = useTheme();
   const { setAccessToken } = useContext(TokenContext);
   const { setCurrentUser } = useContext(UserContext);
   const navigate = useNavigate();
@@ -19,13 +25,29 @@ export function Logout() {
   return (
     <>
       <Header />
-      <Button
-        onClick={handleClick}
-        variant="outlined"
-        sx={{ my: 2, width: 90 }}
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        padding={4}
       >
-        Logout
-      </Button>
+        <SvgIcon
+          sx={{
+            fontSize: 150,
+            fill: theme.palette.background.default,
+            stroke: theme.palette.primary.light,
+            strokeWidth: 2,
+          }}
+          component={Warning}
+        />
+        <Button
+          onClick={handleClick}
+          variant="outlined"
+          sx={{ my: 2, width: 90 }}
+        >
+          Logout
+        </Button>
+      </Box>
     </>
   );
 }
